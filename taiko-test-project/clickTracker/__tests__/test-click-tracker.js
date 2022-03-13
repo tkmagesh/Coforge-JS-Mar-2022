078a66e387c930350d961ee27bbb67f7c656cd6a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-13 13:14:47 +0530

    updates

--- taiko-test-project/clickTracker/__tests__/test-click-tracker.js
+++ taiko-test-project/clickTracker/__tests__/test-click-tracker.js
@@ -17,11 +17,25 @@ const { openBrowser, goto, text, click, below, closeBrowser } = require('taiko')
 })(); 
 */
 describe('Click Tracker', () => {
-    it('Should have [count] as the default counter value', async () => {
+    //beforeEach - will execute the given function BEFORE executing each of the 'it' functions
+    beforeEach(async () => {
         await openBrowser()
         await goto('http://localhost:8080/clickTracker.html')
+    })
+
+    //afterEach - will execute the given function AFTER executing each of the 'it' functions
+    afterEach(async () => {
+        await closeBrowser()
+    })
+
+    it('Should have [count] as the default counter value', async () => {
         const result = await text('[count]').exists();
         expect(result).toBeTruthy()
-        await closeBrowser();
+    })
+
+    it('Should display 1 after "Track" button click', async () => {
+        await click('Track');
+        const result = text('1', below('Track')).exists()
+        expect(result).toBeTruthy()
     })
 })
